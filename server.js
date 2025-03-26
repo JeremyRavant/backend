a0c7f876f2fb528dc62d0943dd5468c024b8ca00
Jeremy Ravant <jeremy.ravant@outlook.fr>
2025-03-26 22:28:34 +0100

    move port and listen to server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const {app} = require('./config/app')
-const { signUp, logIn} = require ('./controllers/users.controller')
 const { booksRouter} = require('./controllers/books.controller')
 const { usersRouter} = require('./controllers/users.controller')
+
+const PORT = process.env.PORT || 4000
+
 
 app.get('/', (req, res) => res.send("server running!"))
 
 app.use("/api/auth", usersRouter)
 app.use("/api/books", booksRouter)
+
+
+app.listen(PORT, function(){
+    console.log(`Server is running on ${PORT}`)
+})
